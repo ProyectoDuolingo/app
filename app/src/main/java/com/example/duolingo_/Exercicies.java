e91c5f36480586ca128f5bcc433583254fcc5ec5
Dani <danielnupa0904@gmail.com>
2020-12-01 15:12:24 +0100

    New method to open different activities from cursos

---
+++ app/src/main/java/com/example/duolingo_/Exercicies.java
@@ -0,0 +1,14 @@
+package com.example.duolingo_;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Exercicies extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_exercicies);
+    }
+}
---
+++ app/src/main/java/com/example/duolingo_/Exercicies5.java
@@ -0,0 +1,14 @@
+package com.example.duolingo_;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Exercicies5 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_exercicies5);
+    }
+}
